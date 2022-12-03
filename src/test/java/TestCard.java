6bd1687441feb22be46c2ebe683412ff60f6c844
julyy5 <smirnova-1999@inbox.ru>
2022-12-03 14:27:14 +0300

    update test

--- src/test/java/TestCard.java
+++ src/test/java/TestCard.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
